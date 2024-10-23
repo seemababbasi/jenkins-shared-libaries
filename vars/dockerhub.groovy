a9ecd4b66958eb934374b6e10c45a44cade070bf
seemababbasi <39179880+seemababbasi@users.noreply.github.com>
2024-10-23 11:53:37 +0500

    Update dockerhub.groovy

--- vars/dockerhub.groovy
+++ vars/dockerhub.groovy
@@ -7,3 +7,5 @@ def call(){
   sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPassword} "
   sh "docker image tag notesapp:latest ${env.dockerHubUser}/notes-app:latest"
   sh "docker push ${env.dockerHubUser}/notes-app:latest"
+                }
+}
