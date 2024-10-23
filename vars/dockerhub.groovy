a67046945d93bc0c756badec06d29fff173b46e7
seemababbasi <39179880+seemababbasi@users.noreply.github.com>
2024-10-23 13:45:19 +0500

    Update dockerhub.groovy

--- vars/dockerhub.groovy
+++ vars/dockerhub.groovy
@@ -5,7 +5,6 @@ def call(String ProjectName, String ImageTag, String dockerHubUser){
                 usernameVariable:"dockerHubUser",
                 passwordVariable:"dockerHubPassword")]){
   sh "docker login -u ${dockerHubUser} -p ${dockerHubPassword} "
-  sh "docker image tag  ${ProjectName}:${ImageTag}  ${dockerHubUser}/${ProjectName}:${ImageTag}"
   sh "docker push ${dockerHubUser}/${ProjectName}:{ImageTag}"
                 }
 }
