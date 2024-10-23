79303c9bf35296d5a4a1ffa351f6ba83a54b6057
seemababbasi <39179880+seemababbasi@users.noreply.github.com>
2024-10-23 12:28:14 +0500

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,5 @@
 def call(){
-                echo 'I am cloning the code'
-                git url: "https://github.com/seemababbasi/django-notes-app.git", branch: "main"
-                echo "cloining the code successfully"
+            echo 'I am cloning the code'
+            git url: "https://github.com/seemababbasi/django-notes-app.git", branch: "main"
+            echo "cloining the code successfully"
 }
